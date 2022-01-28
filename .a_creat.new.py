6ec6343629c3953158dc9c5ed7518c42e82be211
peak-neo <yaml.ide.xml@foxmail.com>
2022-01-29 07:25:06 +0800

    update

--- .a_creat.new.py
+++ .a_creat.new.py
@@ -7,24 +7,24 @@ def create():
     title = input("标题(尽量使用字母而非汉字或其它字符): ")
     ftitleList = title.split()
     ftitle = ""
-    print(ftitleList)
-    # for i in (0,(len(ftitleList)-1)):
-    #     temp = ftitleList[i]
-    #     if i != (len(ftitleList) - 1):
-    #         ftitle = ftitle + temp + "-" 
-    #     else:
-    #         ftitle = ftitle + temp
-    # t = time.strftime("%Y-%m-%d",time.localtime())
-    # filename = t + "-" + ftitle + ".md"
-    # print(filename)
-    # '''创建新博客文件'''
-    # if not os.path.exists(filename):
-    #     f = open("./_posts/drafts/" + filename,"w")
-    #     f.close()
-    #     print(filename + " created")
-    # else:
-    #     print(filename + " already existed.")
-    # return
+    for i in (0,len(ftitleList)):
+        print(i)
+        # temp = ftitleList[i]
+        # if i != (len(ftitleList) - 1):
+        #     ftitle = ftitle + temp + "-" 
+        # else:
+        #     ftitle = ftitle + temp
+    t = time.strftime("%Y-%m-%d",time.localtime())
+    filename = t + "-" + ftitle + ".md"
+    print(filename)
+    '''创建新博客文件'''
+    if not os.path.exists(filename):
+        f = open("./_posts/drafts/" + filename,"w")
+        f.close()
+        print(filename + " created")
+    else:
+        print(filename + " already existed.")
+    return
 
 create()
 
