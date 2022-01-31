43ad50a68124eb396748c71a583df50e25f4b5b7
peak-neo <yaml.ide.xml@foxmail.com>
2022-01-31 15:27:15 +0800

    update

--- .a_creat.new.py
+++ .a_creat.new.py
@@ -1,30 +1,50 @@
 import time
 import os
 
-def create():
+# def create():
 
+#     title = input("标题(尽量使用字母而非汉字或其它字符): ")
+#     ftitleList = title.split()
+#     ftitle = ""
+#     for i in range(0,len(ftitleList)):
+#         temp = ftitleList[i]
+#         if i != (len(ftitleList) - 1):
+#             ftitle = ftitle + temp + "-" 
+#         else:
+#             ftitle = ftitle + temp
+#     t = time.strftime("%Y-%m-%d",time.localtime())
+#     filename = t + "-" + ftitle + ".md"
+    
+#     '''创建新博客文件'''
+#     if not os.path.exists(filename):
+#         f = open("./_posts/drafts/" + filename,'w')
+#         print(filename)
+#         f.close()
+#         print(filename + " created")
+#     else:
+#         print(filename + " already existed.")
+#     return
 
-    title = input("标题(尽量使用字母而非汉字或其它字符): ")
-    ftitleList = title.split()
-    ftitle = ""
-    for i in range(0,len(ftitleList)):
-        temp = ftitleList[i]
-        if i != (len(ftitleList) - 1):
-            ftitle = ftitle + temp + "-" 
-        else:
-            ftitle = ftitle + temp
-    t = time.strftime("%Y-%m-%d",time.localtime())
-    filename = t + "-" + ftitle + ".md"
-    print(filename)
-    '''创建新博客文件'''
-    if not os.path.exists(filename):
-        f = open("./_posts/drafts/" + filename,"w")
-        f.close()
-        print(filename + " created")
+# create()
+
+title = input("标题(尽量使用字母而非汉字或其它字符): ")
+ftitleList = title.split()
+ftitle = ""
+for i in range(0,len(ftitleList)):
+    temp = ftitleList[i]
+    if i != (len(ftitleList) - 1):
+        ftitle = ftitle + temp + "-" 
     else:
-        print(filename + " already existed.")
-    return
-
-create()
-
-os.system(".a_update_blog.bat")
+        ftitle = ftitle + temp
+t = time.strftime("%Y-%m-%d",time.localtime())
+filename = t + "-" + ftitle + ".md"
+head = f'''
+---
+    layout: article
+    sharing: true
+    date: {t}
+    author: Neolux Lee
+    tags: Unclassified
+---
+'''
+os.system(f"echo {head} >> /_posts/drafts/{filename}")
