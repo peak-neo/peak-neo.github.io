63d25ca1539c30a0ae815a362feb3cb579baac64
peak-neo <yaml.ide.xml@foxmail.com>
2022-02-02 12:39:18 +0800

    update

--- .a.publish.article.py
+++ .a.publish.article.py
@@ -1,1 +1,20 @@
 import os
+
+path='./_posts/drafts'
+
+files=os.listdir(path)
+drafts = {}
+for file in files:
+    drafts[str(files.index(file))] = file
+print(drafts)
+
+for i,f in drafts.items():
+    print(f"{i}: {f}")
+
+will_pub = drafts[input("输入序号： ")]
+confirm=input(f"{will_pub}将会被发布，请输入Y(es)确认：")
+if confirm == "Y" or "Yes":
+    os.system(f"move .\_posts\drafts\{will_pub} .\_posts\  ")
+    print(f'{will_pub} 已经发布。')
+else:
+    print("Canceled")
---
+++ _posts/drafts/2022-02-02-hello-world.md
@@ -0,0 +1,8 @@
+---
+    layout: article
+    sharing: true
+    date: 2022-02-02
+    title: hello-world
+    author: Neolux Lee
+    tags: Unclassified
+---
